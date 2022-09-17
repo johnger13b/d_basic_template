f4e06c54daf6cb729e26d1d549a3423ba94b1bd0
johnger13b <johngg@uninorte.edu.co>
2022-09-16 21:45:52 -0500

    udate

--- lib/converter.dart
+++ lib/converter.dart
@@ -1,9 +1,11 @@
 abstract class Converter {
   static String bin2dec(String binary) {
-    // TODO: convierte [binary] en un [int] de base 2 y luego conviertelo a base 10
+    // convierte [binary] en un [int] de base 2 y luego conviertelo a base 10
+    return int.parse(binary, radix: 2).toRadixString(10);
   }
 
   static String dec2bin(String decimal) {
-    // TODO: convierte [decimal] en un [int] de base 10 y luego conviertelo a base 2
+    // convierte [decimal] en un [int] de base 10 y luego conviertelo a base 2
+    return int.parse(decimal, radix: 10).toRadixString(2);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,32 @@
 import 'package:dart_basics/converter.dart';
 
 class MainConverter {
-  // TODO: Define dos varibles privadas String nulables: [binary] y [decimal]
+  // Define dos varibles privadas String nulables: [binary] y [decimal]
+  String? _binary, _decimal;
 
   MainConverter() {
-    // TODO: Inicializa [binary] y [decimal] en ['0'] solo cuando sean nulos
-    // TODO-HINT: Null-sound assignment
+    // Inicializa [binary] y [decimal] en ['0'] solo cuando sean nulos
+    // -HINT: Null-sound assignment
+    _binary ??= '0';
+    _decimal ??= '0';
   }
 
-  // TODO: Crea un [setter] para [binary] con el parametro nulable.
-
-  // TODO: Crea un [setter] para [decimal] con el parametro nulable.
+  // Crea un [setter] para [binary] con el parametro nulable.
+  set binary(String? value) => _binary = value;
+  // Crea un [setter] para [decimal] con el parametro nulable.
+  set decimal(String? value) => _decimal = value;
 
   String convertBinary() {
-    // TODO: Asegurate que [binary] no es nulo antes de intentar la conversion.
-    // TODO: Usa [Converter] para convertir el numero binario a un numero decimal.
+    // Asegurate que [binary] no es nulo antes de intentar la conversion.
+    assert(_binary != null);
+    // Usa [Converter] para convertir el numero binario a un numero decimal.
+    return Converter.bin2dec(_binary!);
   }
 
   String convertDecimal() {
-    // TODO: Asegurate que [decimal] no es nulo antes de intentar la conversion.
-    // TODO: Usa [Converter] para convertir el numero decimal a un numero binario.
+    // Asegurate que [decimal] no es nulo antes de intentar la conversion.
+    assert(_binary != null);
+    // Usa [Converter] para convertir el numero decimal a un numero binario.
+    return Converter.dec2bin(_decimal!);
   }
 }
